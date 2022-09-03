3aa54f623f2173375dfcd34a5d5f0f43b3c05f0f
naufalrays <rayhannaufalherlano@gmail.com>
2022-09-03 10:33:04 +0700

    Mengerjakan Praktikum Section 8

--- 8_Advance Function - Async-Await/praktikum/list_map.dart
+++ 8_Advance Function - Async-Await/praktikum/list_map.dart
@@ -1,10 +1,11 @@
 void main(List<String> args) {
   List a = [
-    [1, 30],
-    [2, 20]
+    ['Nama', 'Rayhan'],
+    ['Umur', 21]
   ];
 
   Map k_list = {};
   k_list[a[0][0]] = a[0][1];
+  k_list[a[1][0]] = a[1][1];
   print(k_list);
 }
